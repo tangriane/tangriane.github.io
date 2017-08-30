aa8ebff162151e011f8ed3774bed4ad0de2e375d
tangriane <tangrianekolisnichenco@hotmail.com>
2017-08-30 13:40:21 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,75 @@
 var gulp        = require('gulp'),
     sass        = require('gulp-ruby-sass'),
+    babel       = require('gulp-babel'),
+    cssmin = require("gulp-cssmin"), // Minifica o CSS
+    minify = require('gulp-minify'), // Minifica o JS
     imagemin    = require('gulp-imagemin'),
     changed     = require('gulp-changed'),
+    
     browserSync = require('browser-sync');
     deploy = require("gulp-gh-pages");
  	uglify = require('gulp-uglify');
-    babel = require('gulp-babel');
+
+
+
 
 gulp.task('sass', function () {
     gulp.src('./assets/sass/*.scss')
         .pipe(sass({compass: true}))
         .on('error', function (err) { console.log(err.message); })
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('./assents/css'));
 });
  
-gulp.task('jpg', function() {
-    gulp.src('./assets/img/**/*.jpg')
-        .pipe(changed('./dist/img/'))
-        .pipe(imagemin({
-            progressive: true
-        }))
-        .pipe(gulp.dest('./dist/img/'));
+//es6
+gulp.task("babel", function () {
+return gulp.src("./assets/babel/*.js")
+.pipe(sourcemaps.init())
+.pipe(babel())
+.pipe(concat("babel.js"))
+.pipe(sourcemaps.write("."))
+.pipe(gulp.dest("./js"));
 });
- 
-gulp.task('browser-sync', function() {
+
+// Processo que agrupará todos os arquivos CSS, removerá comentários CSS e minificará.
+gulp.task('minify-css', function(){
+    gulp.src('./assents/css')
+    .pipe(concat('style.min.css'))
+    .pipe(stripCssComments({all: true}))
+    .pipe(cssmin())
+    .pipe(gulp.dest('./css'));
+});
+
+//minificação do js
+gulp.task('compress', function() {
+  gulp.src('js/*.js')
+    .pipe(minify({
+        ext:{
+            src:'-debug.js',
+            min:'.js'
+        },
+        exclude: ['tasks'],
+        ignoreFiles: ['.combo.js', '-min.js']
+    }))
+    .pipe(gulp.dest('dist'))
+});
+
+
+
+gulp.task('imagemin', function() {
+   var imgSrc = 'src/images/*.+(png|jpg|gif)',
+   imgDst = 'build/images';
+   
+   gulp.src(imgSrc)
+   .pipe(changed(imgDst))
+   .pipe(imagemin())
+   .pipe(gulp.dest(imgDst));
+});
+
+gulp.task('default',['imagemin'],function(){
+});
+
+
+ gulp.task('browser-sync', function() {
     browserSync.init(['./dist/css/**', './views/**'], {
         server: {
             baseDir: './',
@@ -31,50 +77,23 @@ gulp.task('browser-sync', function() {
         }
     });
 });
- 
 gulp.task('watch', ['sass', 'browser-sync'], function () {
     gulp.watch('./assets/sass/**/*.scss', ['sass']);
 });
 
 
-
+//jekyll
 gulp.task("deploy", ["jekyll-build"], function () {
     return gulp.src("./_site/**/*")
         .pipe(deploy());
 });
 
-//minificação javascript
 
-gulp.task('scripts', function() {
-    // corpo da tarefa 
-    return gulp
-            .src(['src/js/**/*.js'])
-            .pipe(uglify())
-            .pipe(gulp.dest('build/js'));      
-});
 
-var css = [
- './src/css/*.css',
- './src/style.css'
-];
 
-// Processo que agrupará todos os arquivos CSS, removerá comentários CSS e minificará.
-gulp.task('minify-css', function(){
-    gulp.src(css)
-    .pipe(concat('style.min.css'))
-    .pipe(stripCssComments({all: true}))
-    .pipe(cssmin())
-    .pipe(gulp.dest('./build/'));
-});
-//es6
-gulp.task("babel", function () {
-return gulp.src("babel/*.js")
-.pipe(sourcemaps.init())
-.pipe(babel())
-.pipe(concat("babel.js"))
-.pipe(sourcemaps.write("."))
-.pipe(gulp.dest("js"));
-});
+
+
+
 
 
 //task default gulp
